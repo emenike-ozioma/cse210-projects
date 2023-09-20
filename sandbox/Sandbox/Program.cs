c23a6a73089c0910378d94e206dd08535268fade
emenike-ozioma <emenikeozioma227@gmail.com>
2023-09-20 22:48:31 +0100

    stuffs to remember in prep3

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        // for loop(python for x in range) 
+        // counting from 2 to 20 by two's:
+        for (int i = 2; i <= 20; i = i + 2 )
+        {
+            Console.WriteLine(i);
+        }
+        
+        // get random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
     }
 }
