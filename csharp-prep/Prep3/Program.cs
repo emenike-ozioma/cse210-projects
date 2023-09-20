b19c3907017bac3027a2037c06e1024ec28709e0
emenike-ozioma <emenikeozioma227@gmail.com>
2023-09-20 22:45:58 +0100

    guess my number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            string guessed;
+            int track = 0;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                int guess = int.Parse(answer);
+
+                if (guess > magicNumber)
+                {
+                    guessed = "Lower";
+                }
+                else if (guess < magicNumber)
+                {
+                    guessed = "Higher";
+                }
+                else
+                {
+                    guessed = "You guessed it!";
+                }
+
+                Console.WriteLine(guessed);
+                track ++;
+
+            } while (guessed != "You guessed it!");
+
+            Console.WriteLine($"You guessed {track} times.");
+
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+        }
     }
 }
