4bf43b714eddf5347d1a8b0d18f522c8ecaef347
emenike-ozioma <emenikeozioma227@gmail.com>
2023-09-26 16:13:21 +0100

    prep05 learning function basics

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,46 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    { 
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        
+        return userNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        number *= number;
+        return number;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
