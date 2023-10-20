c9c6420f48baa1c53e5ea36d1dcc0bcadea56d07
emenike-ozioma <emenikeozioma227@gmail.com>
2023-10-20 07:33:03 +0100

    praticing encapulation

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,55 @@
+using System;
+
+public class Fraction
+{
+    // Attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        // Default to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop ()
+    {
+        return _top;
+    }
+    public void SetTop (int top)
+    {
+        _top = top;
+    }
+    public int GetBottom ()
+    {
+       return _bottom;
+    }
+    public void SetBottom (int bottom)
+    {
+        _bottom = bottom; 
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    public double GetDecimalValue ()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
