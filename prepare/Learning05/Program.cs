507b2611f651b02020763a3735bff76308b623b1
emenike-ozioma <emenikeozioma227@gmail.com>
2023-11-11 01:31:19 +0100

    Polymorphism Learning with shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapesList = new List<Shape>();
+
+        Square s1 = new Square("Blue", 10);
+        shapesList.Add(s1);
+
+        Rectangle s2 = new Rectangle("Black", 5, 5);
+        shapesList.Add(s2);
+
+        Circle s3 = new Circle("Blue", 2);
+        shapesList.Add(s3);
+
+        foreach (Shape shape in shapesList)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
